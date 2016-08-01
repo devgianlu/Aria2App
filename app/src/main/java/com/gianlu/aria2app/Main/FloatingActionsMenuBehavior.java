d44cfd7de289d3c12774b26943eae83f3fe719b7
Gianlu <altomanigianluca@gmail.com>
2016-08-01 11:56:24 +0200

    Backup commit before MoreAboutDownload re-layout

--- app/src/main/java/com/gianlu/aria2app/Main/FloatingActionsMenuBehavior.java
+++ app/src/main/java/com/gianlu/aria2app/Main/FloatingActionsMenuBehavior.java
@@ -1,11 +1,13 @@
 package com.gianlu.aria2app.Main;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
 import android.view.View;
 
+@SuppressLint("unused")
 public class FloatingActionsMenuBehavior extends CoordinatorLayout.Behavior {
 
     public FloatingActionsMenuBehavior(Context context, AttributeSet attrs) {
