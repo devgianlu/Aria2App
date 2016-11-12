0acdf29a8e2a1dcfbb258b733eba14d3215aac29
Gianlu <altomanigianluca@gmail.com>
2016-11-12 21:23:59 +0100

    Vote dialog (still not reviewed)

--- app/src/main/java/com/gianlu/aria2app/Google/Analytics.java
+++ app/src/main/java/com/gianlu/aria2app/Google/Analytics.java
@@ -11,7 +11,7 @@ import com.google.android.gms.analytics.Tracker;
 
 public class Analytics {
     public static final String CATEGORY_USER_INPUT = "User input";
-    public static final String ACTION_DOWNLOAD_FILE = "Downloading file";
+    public static final String ACTION_DOWNLOAD_FILE = "Download file";
     public static final String ACTION_NEW_PROFILE = "New profile";
     public static final String ACTION_DELETE_PROFILE = "Profile deleted";
     public static final String ACTION_CHANGED_GLOBAL_OPTIONS = "Global options changed";
--- app/src/main/java/com/gianlu/aria2app/MainActivity.java
+++ app/src/main/java/com/gianlu/aria2app/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -714,6 +715,38 @@ public class MainActivity extends AppCompatActivity {
             startService(NotificationService.createStartIntent(this));
         } else {
             stopService(new Intent(this, NotificationService.class).setAction("STOP"));
+        }
+
+        long firstStart = sharedPreferences.getLong("firstStart", -1);
+        if (firstStart == -1) {
+            sharedPreferences.edit()
+                    .putLong("firstStart", System.currentTimeMillis())
+                    .apply();
+        } else if (firstStart > 0) {
+            if (System.currentTimeMillis() - sharedPreferences.getLong("firstStart", -1) >= 259200000) {
+                CommonUtils.showDialog(this, new AlertDialog.Builder(this)
+                        .setTitle(R.string.voteApp)
+                        .setMessage(R.string.voteApp_message)
+                        .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                try {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + getApplicationContext().getPackageName())));
+                                } catch (android.content.ActivityNotFoundException ex) {
+                                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + getApplicationContext().getPackageName())));
+                                }
+                            }
+                        })
+                        .setNegativeButton(android.R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                            }
+                        }));
+
+                sharedPreferences.edit()
+                        .putLong("firstStart", -2)
+                        .apply();
+            }
         }
     }
 
