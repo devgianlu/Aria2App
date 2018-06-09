2349d25eae842e8e572a79203bfb90ef0b6853f5
Gianlu <altomanigianluca@gmail.com>
2018-06-09 11:39:58 +0200

    Finally fixed FileTypeTextView

--- app/src/main/java/com/gianlu/aria2app/FileTypeTextView.java
+++ app/src/main/java/com/gianlu/aria2app/FileTypeTextView.java
@@ -1,19 +1,30 @@
 package com.gianlu.aria2app;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.widget.TextViewCompat;
-import android.support.v7.widget.AppCompatTextView;
+import android.text.Layout;
+import android.text.StaticLayout;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.TypedValue;
-import android.view.Gravity;
+import android.view.View;
 
 import com.gianlu.commonutils.FontsManager;
 
 import java.util.Objects;
 
-public class FileTypeTextView extends AppCompatTextView { // FIXME: Still having issues
+public class FileTypeTextView extends View {
+    private final TextPaint textPaint;
+    private final float mDefaultTextSize;
+    private final int mMaxHeight;
+    private String mText;
+    private StaticLayout mLayout;
+
     public FileTypeTextView(Context context) {
         this(context, null, 0);
     }
@@ -24,11 +35,28 @@ public class FileTypeTextView extends AppCompatTextView { // FIXME: Still having
 
     public FileTypeTextView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setTypeface(FontsManager.get().get(context, FontsManager.ROBOTO_BLACK));
-        TextViewCompat.setAutoSizeTextTypeUniformWithConfiguration(this, 8, 36, 1, TypedValue.COMPLEX_UNIT_SP);
-        setGravity(Gravity.CENTER);
+        setWillNotDraw(false);
+
+        textPaint = new TextPaint();
+        textPaint.setTypeface(FontsManager.get().get(context, FontsManager.ROBOTO_BLACK));
+        textPaint.setAntiAlias(true);
+
+        mDefaultTextSize = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 100, context.getResources().getDisplayMetrics());
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.FileTypeTextView, defStyleAttr, 0);
+        try {
+            mMaxHeight = a.getDimensionPixelSize(R.styleable.FileTypeTextView_maxHeight, (int) mDefaultTextSize);
+            textPaint.setColor(a.getColor(R.styleable.FileTypeTextView_textColor, Color.BLACK));
+        } finally {
+            a.recycle();
+        }
 
         if (isInEditMode()) setExtension("XML");
+    }
+
+    public static float getFitTextSize(@NonNull TextPaint paint, float width, @NonNull String text) {
+        float nowWidth = paint.measureText(text);
+        return width / nowWidth * paint.getTextSize();
     }
 
     public void setFilename(@NonNull String filename) {
@@ -37,8 +65,33 @@ public class FileTypeTextView extends AppCompatTextView { // FIXME: Still having
         else setExtension(null);
     }
 
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (mText != null) {
+            int width = MeasureSpec.getSize(widthMeasureSpec);
+            textPaint.setTextSize(mDefaultTextSize);
+            textPaint.setTextSize(Math.min(getFitTextSize(textPaint, width, mText), mMaxHeight) - 1);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                mLayout = StaticLayout.Builder.obtain(mText, 0, mText.length(), textPaint, width)
+                        .setAlignment(Layout.Alignment.ALIGN_CENTER)
+                        .setIncludePad(false).build();
+            } else {
+                mLayout = new StaticLayout(mText, textPaint, width, Layout.Alignment.ALIGN_CENTER, 1, 0, false);
+            }
+
+            int height = Math.min(mLayout.getHeight() - mLayout.getTopPadding() - mLayout.getBottomPadding(), mMaxHeight);
+            super.onMeasure(widthMeasureSpec, MeasureSpec.makeMeasureSpec(height, MeasureSpec.EXACTLY));
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        mLayout.draw(canvas);
+    }
+
     public void setExtension(@Nullable String ext) {
-        if (Objects.equals(getText().toString(), ext)) return;
+        if (Objects.equals(mText, ext)) return;
         if (ext == null) {
             ext = "???";
         } else {
@@ -46,6 +99,8 @@ public class FileTypeTextView extends AppCompatTextView { // FIXME: Still having
             if (ext.length() > 4) ext = ext.substring(0, 4);
         }
 
-        setText(ext.toUpperCase());
+        mText = ext.toUpperCase();
+        requestLayout();
+        invalidate();
     }
 }
